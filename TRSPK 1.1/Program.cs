9b970e3b86ccd4dca98f17ab8a229b11002c6d1e
lluxfero <onastja02@gmail.com>
2022-09-13 00:06:24 +0300

    1.1 1.2

--- TRSPK 1.1/Program.cs
+++ TRSPK 1.1/Program.cs
@@ -15,6 +15,32 @@ var FirstWorker = new Worker();
 s = FirstWorker.ToString();
 Console.WriteLine(s);
 
+
+Console.WriteLine("======StringList======");
+var FirstStringList = new StringList();
+FirstStringList.Insert("1");
+FirstStringList.Insert("2");    
+FirstStringList.Insert("3");    
+FirstStringList.Insert("4");    
+FirstStringList.Insert("5");
+FirstStringList.Insert("6");
+FirstStringList.Print();
+
+Console.WriteLine("_Delete(4)");
+FirstStringList.Delete(4);
+FirstStringList.Print();
+
+Console.WriteLine("_Search(\"3\")");
+Console.WriteLine(FirstStringList.Search("3"));
+
+Console.WriteLine("_Update(1, \"7\")");
+FirstStringList.Update(1, "7");
+FirstStringList.Print();
+
+Console.WriteLine("_GetAt(0)");
+Console.WriteLine(FirstStringList.GetAt(0));
+
+
 Console.WriteLine("\nPress <Enter>");
 Console.ReadLine();
 
@@ -135,5 +161,48 @@ class Worker : Man
     public override string ToString()
     {
         return $"Работник, {name}, {age}, Место работы: {work}";
+    }
+}
+
+class StringList
+{
+    public int size;
+    string[] masstr = new string[100];
+    public void Insert(string s) 
+    {
+        masstr[size] = s;
+        size++;
+    }
+    public void Delete(int n)
+    {
+        if (n >= 0 && n < size)
+        {
+            for (int i = n; i < size - 1; i++)
+                masstr[i] = masstr[i + 1];
+            size--;
+        }
+        else Console.WriteLine($"Ошибка! Элемента с индексом {n} нет");
+    }
+    public int Search(string s)
+    {
+        for (int i = 0; i < size; i++)
+            if (masstr[i] == s) return i;
+        return -1;
+    }
+    public void Update(int n, string s)
+    {
+        if (n >= 0 && n < size) masstr[n] = s;
+        else Console.WriteLine($"Ошибка! Элемента с индексом {n} нет");
+    }
+    public string GetAt(int n)
+    {
+        if (n >= 0 && n < size) return masstr[n];
+        return null;
+    }
+    public void Print()
+    {
+        Console.WriteLine("__print__");
+        for (int i = 0; i < size; i++)
+            Console.WriteLine($"Индекс {i}, Значение: {masstr[i]}, Размер: {size}");
     }
 }
